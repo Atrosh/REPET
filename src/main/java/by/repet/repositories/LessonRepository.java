299a9f880d12d29bf493af51d5e3f945a8557c26
Vlad <vladru08@gmail.com>
2017-05-29 00:59:48 +0300

    fix lessons api

--- src/main/java/by/repet/repositories/LessonRepository.java
+++ src/main/java/by/repet/repositories/LessonRepository.java
@@ -3,15 +3,17 @@ package by.repet.repositories;//Created by vladr on 20.12.2016.
 import by.repet.domain.Course;
 import by.repet.domain.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
-    Collection<Lesson> findByUserIdAndDateBetween(long userId, Date start, Date end);
+    Collection<Lesson> findByDateBetweenAndUserId(Date start, Date end, long userId);
 
-    Collection<Lesson> findByUserIdOrCourseInAndDateBetween(long userId, List<Course> courses, Date start, Date end);
+    @Query("SELECT l FROM Lesson l WHERE l.date BETWEEN ?1 AND ?2 AND (l.user.id = ?3 or l.course.id in ?4)")
+    Collection<Lesson> findByDateBetweenAndUserIdAndCourseIn(Date start, Date end, long userId, List<Long> courses);
 
     Collection<Lesson> findByDateBetweenAndCourseIn(Date start, Date end, List<Course> courses);
 
--- src/main/java/by/repet/services/Impl/LessonServiceImpl.java
+++ src/main/java/by/repet/services/Impl/LessonServiceImpl.java
@@ -1,6 +1,7 @@
 package by.repet.services.Impl;//Created by vladr on 20.12.2016.
 
 import by.repet.common.LessonDto;
+import by.repet.domain.Course;
 import by.repet.domain.Lesson;
 import by.repet.domain.User;
 import by.repet.repositories.LessonRepository;
@@ -15,7 +16,9 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
 import java.util.stream.Collectors;
 
@@ -42,7 +45,8 @@ public class LessonServiceImpl implements LessonService {
         User user = userContextService.getCurrentUser();
         Date since = DateTime.now().withTimeAtStartOfDay().toDate();
         Date till = DateTime.now().withTimeAtStartOfDay().plusDays(1).toDate();
-        return lessonRepository.findByUserIdOrCourseInAndDateBetween(user.getId(), user.getCourses(), since, till);
+        return lessonRepository.findByDateBetweenAndUserIdAndCourseIn(since, till, user.getId(),
+                getCourseId(user));
     }
 
     @Override
@@ -51,9 +55,9 @@ public class LessonServiceImpl implements LessonService {
         ArrayList<Collection<Lesson>> days = new ArrayList<>();
         Calendar c = Calendar.getInstance();
         c.setTime(new Date());
-        Collection<Lesson> lessons = lessonRepository.findByUserIdOrCourseInAndDateBetween(
-                user.getId(), user.getCourses(),
-                DateTime.now().minusDays(7).toDate(), DateTime.now().plusDays(7).toDate());
+        Collection<Lesson> lessons = lessonRepository.findByDateBetweenAndUserIdAndCourseIn(
+                DateTime.now().minusDays(7).toDate(), DateTime.now().plusDays(7).toDate(),
+                user.getId(), getCourseId(user));
         for (int i = 1; i < 8; i++) {
             c.set(Calendar.DAY_OF_WEEK, i + 1);
             days.add(lessons.stream()
@@ -83,5 +87,13 @@ public class LessonServiceImpl implements LessonService {
     @Override
     public void remove(Long lessonId) {
         lessonRepository.delete(lessonId);
+    }
+
+    private List<Long> getCourseId(User user) {
+        if (user.getCourses() == null || user.getCourses().isEmpty()) {
+            return Collections.singletonList(0L);
+        } else {
+            return user.getCourses().stream().map(Course::getId).collect(Collectors.toList());
+        }
     }
 }
